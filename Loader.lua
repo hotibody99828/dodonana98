94d64cef5773245832bb895db21eca3ca1a577a7
hotibody99828 <lyybigber1616@gmail.com>
2026-09-08 08:54:16 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@
 -- YOKUDO HUB | SEA2 | [Premium] | Loader (NO CONFIG - CLEAN)
 -- ==================================================
 
-local BASE_URL = "https://raw.githubusercontent.com/hotibody99828/yokudo/dodonana98/"
+local BASE_URL = "https://raw.githubusercontent.com/hotibody99828/dodonana98/main/"
 
 _G.YOKUDO_EnablePrint = false
 
